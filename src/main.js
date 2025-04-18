ff18f2ab168d47e5b579807f395f8aa8f039aa46
Angga Nurhiman <nurhimanangga@gmail.com>
2025-04-18 14:03:18 +0700

    :bug: Fix styles not implemented

--- src/main.js
+++ src/main.js
@@ -1,11 +1,21 @@
-import { createApp } from 'vue'
-import MyButton from './components/MyButtonWidget.vue'
+import { createApp } from 'vue';
+import MyButton from './components/MyButtonWidget.vue';
+import style from './MyButtonWidget.module.css?inline';
+
+function injectStyle(css) {
+    if (typeof document !== 'undefined') {
+        const styleEl = document.createElement('style');
+        styleEl.appendChild(document.createTextNode(css));
+        document.head.appendChild(styleEl);
+    }
+}
 
 function mount(el, props) {
-    createApp(MyButton, props).mount(el)
+    injectStyle(style);
+    createApp(MyButton, props).mount(el);
 }
 
 // Expose to global window object
 if (typeof window !== 'undefined') {
-    window.MyButtonWidgetLib = { mount }
+    window.MyButtonWidgetLib = { mount };
 }
